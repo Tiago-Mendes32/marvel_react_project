db343234bcd2af52b78a935ccab1afd63927ddbf
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-11-22 13:13:35 -0300

    Update api keys

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import md5 from "md5";
-import keys from "./keys.json";
 
 const url = "http://gateway.marvel.com/v1/public/characters";
-const publicKey = keys.publicKey;
-const privateKey = keys.privateKey;
+const publicKey = process.env.PUBLIC_KEY;
+const privateKey = process.env.PRIVATE_KEY;
 const ts = Number(new Date());
 const hash = md5(ts + privateKey + publicKey);
 const charactersNumberLimit = 50;
